72a70602e653e5f59f607b38d614d63a32f16a57
Hopd84 <Ca_dict@mail.ru>
2022-11-27 15:18:38 +0300

    Add Constants

---
+++ src/main/java/spb/ifuture/ru/util/Constants.java
@@ -0,0 +1,11 @@
+package spb.ifuture.ru.util;
+
+import lombok.Getter;
+
+@Getter
+public class Constants {
+    public static final Integer TIMEOUT = 6000;
+    public static final String REST_BASE_URL = "http://localhost:8080/api/v1";
+
+    private Constants(){}
+}
