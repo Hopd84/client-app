8d822d8ec77e86b08856d68c3bbb1b9513c86102
Hopd84 <Ca_dict@mail.ru>
2022-11-27 15:20:29 +0300

    Add ClientApplicationTests

---
+++ src/test/java/spb/ifuture/ru/ClientApplicationTests.java
@@ -0,0 +1,13 @@
+package spb.ifuture.ru;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClientApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
