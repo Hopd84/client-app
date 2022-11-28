d501ee629695a0680b4176e9b922f6590589c587
Hopd84 <Ca_dict@mail.ru>
2022-11-28 10:38:20 +0300

    Make Constants final

--- src/main/java/spb/ifuture/ru/util/Constants.java
+++ src/main/java/spb/ifuture/ru/util/Constants.java
@@ -3,7 +3,7 @@ package spb.ifuture.ru.util;
 import lombok.Getter;
 
 @Getter
-public class Constants {
+public final class Constants {
     public static final Integer TIMEOUT = 6000;
     public static final String REST_BASE_URL = "http://localhost:8080/api/v1";
 
